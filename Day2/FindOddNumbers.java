9378a210340ded8cc4bda5c175d42f500c428fd3
Deepalakshmi2210 <deepzthara22@gmail.com>
2026-04-02 09:34:49 +0530

    Add files via upload
    Day2

---
+++ Day2/FindOddNumbers.java
@@ -0,0 +1,14 @@
+package Week1.Day2;
+
+public class FindOddNumbers {
+    
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++){
+            if (i%2==1) {
+                System.out.println("odd Numbers:"+i);    
+            }
+            
+    }
+
+}
+}
---
+++ Day2/MethodsObject.java
@@ -0,0 +1,14 @@
+package Week1.Day2;
+
+public class MethodsObject {
+//Creating Method
+    public void Loginpage(){
+        System.out.println("Login Successfull");
+    }
+//Creating Object to call method
+    public static void main(String[] args) {
+        MethodsObject lp = new MethodsObject();
+        lp.Loginpage();
+    }
+
+}
